37075e9855c975393a3904c864621db0ef530625
sota moriya <sota19990331@hotmail.co.jp>
2019-07-17 20:55:35 +0900

    application html 変更レスポンシブ2

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,4 +1,4 @@
 RakutenWebService.configuration do |c|
-    c.application_id = Application_id
-    c.affiliate_id = Affiliate_id
+    c.application_id = ENV['Application_id']
+    c.affiliate_id = ENV['Affiliate_id']
 end
