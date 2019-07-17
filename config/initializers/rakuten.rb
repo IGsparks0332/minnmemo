855ab4e681c7ec280e9bd989943b422982617349
sota moriya <sota19990331@hotmail.co.jp>
2019-07-17 19:23:02 +0900

    1907171922

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,4 +1,4 @@
 RakutenWebService.configuration do |c|
-    c.application_id = '1079893146530672123'
-    c.affiliate_id = '18bd3bf0.35295f05.18bd3bf1.880974d1'
+    c.application_id = Application_id
+    c.affiliate_id = Affiliate_id
 end
