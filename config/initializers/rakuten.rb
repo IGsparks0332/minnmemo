8306b16ec9a58c53a0830d935ca1fd3b5d9cb879
sota <sota19990331@hotmail.co.jp>
2019-07-23 21:58:28 +0900

    test

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,4 +1,4 @@
-RakutenWebService.configuration do |c|
-    c.application_id = ENV['Application_id']
-    c.affiliate_id = ENV['Affiliate_id']
+RakutenWebService.configure do |config|
+    config.application_id = ENV['Application_id']
+    config.affiliate_id = ENV['Affiliate_id']
 end
