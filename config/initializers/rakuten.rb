a1b92fc63095caeec1dfcd8a393aa7004998a567
sota <sota19990331@hotmail.co.jp>
2019-07-23 22:41:48 +0900

    rakutenAPI

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,4 +1,4 @@
 RakutenWebService.configure do |config|
     config.application_id = ENV['Application_id']
-    config.affiliate_id = ENV['RAKUTEN_AFID']
+    config.affiliate_id = ENV['Affiliate_id']
 end
