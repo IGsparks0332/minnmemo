7538a728129d290d3ad84c6cc84e45011347a2bd
sota <sota19990331@hotmail.co.jp>
2019-07-24 16:59:52 +0900

    荒らし対策 validates

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -4,4 +4,9 @@ class Sentence < ApplicationRecord
   belongs_to :user
   has_many :reactions
   #Timeline End
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :author, presence: true
+  validates :publisher, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,20 @@ class User < ApplicationRecord
   has_many :reacted_sentences, through: :reactions, source: :sentence
   has_many :chat_messages
   has_many :chat_room_users
-  
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+
   def already_reacted?(sentence)
     self.reactions.exists?(sentence_id: sentence.id)
   end
-  
-  
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  
+
+
 end
