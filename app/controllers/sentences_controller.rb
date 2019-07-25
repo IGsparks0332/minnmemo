52f376bb5095967eb63b235fc0c71c9b8220651b
sota <sota19990331@hotmail.co.jp>
2019-07-25 21:40:21 +0900

    googleboolsAPI

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -58,6 +58,12 @@ class SentencesController < ApplicationController
   def show
     @sentence = Sentence.find_by(id: params[:id])
     @items = RakutenWebService::Ichiba::Item.search(:keyword => @sentence.title, :genre_id => "101931" ) # This returns Enamerable object
+    url = 'https://www.googleapis.com/books/v1/volumes?q='
+    request = url + @sentence.title
+    enc_str = URI.encode(request)
+    uri = URI.parse(enc_str)
+    json = Net::HTTP.get(uri)
+    books = JSON.parse(json)
   end
 
   def edit
